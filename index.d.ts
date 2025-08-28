83b91e99b159eaf388a33d1bfb8a01f2e9d6b13b
jalberto-solink <jalberto@solinkcorp.com>
2025-08-28 08:27:09 -0400

    fix: export noonlight type and add missing props to existing

--- index.d.ts
+++ index.d.ts
@@ -4,19 +4,31 @@ declare module "@solinkcorp/addresser" {
 	export function getRandomCity(): { city: string, state: string};
 	export function cities(): IStateCities;
 
+	export interface INoonlightFormat {
+		line1: string;
+		line2?: string;
+		city: string;
+		state?: string;
+		zip: string;
+		country: string;
+	}
+
 	export interface IParsedAddress {
 		zipCode: string;
+		zipCodePlusFour?: string;
 		stateAbbreviation: string;
 		stateName: string;
 		placeName: string;
 		addressLine1: string;
+		addressLine2?: string;
 		streetNumber: string;
 		streetSuffix: string;
 		streetName: string;
+		streetDirection?: string;
+		country: string;
+		countryAbbreviation: string;
+		formattedAddress: string;
 		id: string;
-	}
-
-	export interface IStateCities {
-		[stateName: string]: string[];
+		format(formatType?: string): INoonlightFormat | IParsedAddress;
 	}
 }
